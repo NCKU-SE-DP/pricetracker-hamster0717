ec9e718bd0cd867131bda90a536ef7e72d07988c
hamster <aoscar0717@gmail.com>
2024-10-06 15:21:15 +0800

    feat(RWD):漢堡選單+RWD

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,14 +2,14 @@
     <nav class="navbar">
         <div class="title">
             <RouterLink to="/overview">價格追蹤小幫手</RouterLink>
-            <button @click="openham" >{{opened?'Ｘ':'☰'}}</button>
+            <button @click="openham">{{ opened ? 'Ｘ' : '☰' }}</button>
         </div>
         <ul class="options" v-if="opened">
             <li><RouterLink to="/overview">物價概覽</RouterLink></li>
             <li><RouterLink to="/trending">物價趨勢</RouterLink></li>
             <li><RouterLink to="/news">相關新聞</RouterLink></li>
             <li v-if="!isLoggedIn"><RouterLink to="/login">登入</RouterLink></li>
-            <li v-else @click="logout">Hi, {{getUserName}}! 登出</li>
+            <li v-else @click="logout">Hi, {{ getUserName }}! 登出</li>
         </ul>
     </nav>
 </template>
@@ -24,24 +24,33 @@ export default {
             opened: true
         }
     },
+    mounted() {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize(); 
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    },
     computed: {
-        isLoggedIn(){
+        isLoggedIn() {
             const userStore = useAuthStore();
             return userStore.isLoggedIn;
         },
-        getUserName(){
+        getUserName() {
             const userStore = useAuthStore();
             return userStore.getUserName;
-        },
-        
+        }
     },
     methods: {
-        openham(){
-            this.opened=!this.opened
+        openham() {
+            this.opened = !this.opened;
         },
-        logout(){
+        logout() {
             const userStore = useAuthStore();
             userStore.logout();
+        },
+        handleResize() {
+            this.opened = window.innerWidth >= 768;
         }
     }
 };
@@ -53,7 +62,6 @@ export default {
     justify-content: space-between;
     background-color: #f3f3f3;
     padding: 1.5em;
-    /*height: 4.5em;*/
     width: 100%;
     align-items: center;
     box-shadow: 0 0 5px #000000;
@@ -64,7 +72,7 @@ export default {
     justify-content: space-around;
 }
 
-.title > a{
+.title > a {
     font-size: 1.4em;
     font-weight: bold;
     color: #2c3e50 !important;
@@ -76,40 +84,44 @@ export default {
     font-size: 1.2em;
 }
 
-.navbar li:hover{
+.navbar li:hover {
     cursor: pointer;
     font-weight: bold;
-    background-color:whitesmoke;
+    background-color: whitesmoke;
 }
 
 .navbar a {
     text-decoration: none;
     color: #575B5D;
 }
-.title > button{
+.title > button {
     display: none;
 }
 @media (max-width: 768px) {
-    .navbar li:hover{
-        background-color:rgb(176, 198, 198);
+    .navbar li:hover {
+        background-color: rgb(176, 198, 198);
     }
-    .navbar li{
-        border-radius: 5px;
+    .navbar li {
+        border-top-style: solid;
+        border-color: #dadada;
+        border-width: 0.1px;
         padding: 2px;
+        margin: 0;
+        width: 100vw;
     }
-    .title{
+    .title {
         width: 100%;
         padding: 10px 10px 0px 10px;
         display: flex;
         justify-content: space-between;
     }
-    .navbar{
+    .navbar {
         padding: 0;
     }
-    .navbar,.navbar ul  {
+    .navbar, .navbar ul {
         flex-direction: column;
     }
-    .title > button{
+    .title > button {
         display: contents;
         font-size: 1.4em;
         font-weight: bold;
