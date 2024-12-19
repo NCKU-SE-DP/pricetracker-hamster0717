b2317f75aafba72f5cf8add9f7c895b7efee6e8b
hamster0717 <aoscar0717@gmail.com>
2024-12-19 13:10:41 +0800

    Empty-Commit

--- backend/src/news/config.py
+++ backend/src/news/config.py
@@ -6,11 +6,9 @@ import os
 class NewsSettings(BaseSettings):
     OPENAI_APIKEY: str = os.getenv("OPENAI_APIKEY")
     ANTHROPIC_APIKEY: str = os.getenv("ANTHROPIC_APIKEY")
-
     class Config:
         env_file = Path(__file__).parent.parent / ".env"
         env_file_encoding = "utf-8"
-        env_prefix = "AI_"
 @lru_cache
 def get_NewsSettings() -> NewsSettings:
     return NewsSettings()
