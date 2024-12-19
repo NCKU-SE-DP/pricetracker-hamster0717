9e7bcac7d938659c44ba3b50b7382b62fafa75fa
hamster0717 <aoscar0717@gmail.com>
2024-12-19 13:22:25 +0800

    Empty-Commit

--- backend/src/news/config.py
+++ backend/src/news/config.py
@@ -2,7 +2,10 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 from pathlib import Path
 from functools import lru_cache
+import dotenv
 import os
+
+dotenv.load_dotenv()
 class NewsSettings(BaseSettings):
     OPENAI_APIKEY: str = os.getenv("OPENAI_APIKEY")
     ANTHROPIC_APIKEY: str = os.getenv("ANTHROPIC_APIKEY")
