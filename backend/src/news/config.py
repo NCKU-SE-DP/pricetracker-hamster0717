03f5824aa656c0a91ba0159f94d7288a6bd6d537
hamster0717 <aoscar0717@gmail.com>
2024-12-19 13:56:01 +0800

    fix

--- backend/src/news/config.py
+++ backend/src/news/config.py
@@ -7,8 +7,8 @@ import os
 
 dotenv.load_dotenv()
 class NewsSettings(BaseSettings):
-    OPENAI_APIKEY: str
-    ANTHROPIC_APIKEY: str
+    OPENAI_APIKEY: str = os.getenv("OPENAI_APIKEY", "")
+    ANTHROPIC_APIKEY: str = os.getenv("ANTHROPIC_APIKEY", "")
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
